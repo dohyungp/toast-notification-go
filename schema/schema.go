84bded71e28cd5ffeffdcb4859ea52c526cbab3b
dohyung <dohyung.prk@gmail.com>
2022-01-11 13:01:17 +0900

    ADD 수신자 type에 validation 조건을 추가하다

--- schema/schema.go
+++ schema/schema.go
@@ -7,9 +7,9 @@ package schema
 // TemplateParameter Toast에서 동적으로 문자 템플릿을 만들때 사용하며 Map 타입이다
 // RecipientGroupingKey는 수신자 그룹키이다
 type Recipient struct {
-	RecipientNo              string                 `json:"recipientNo"`
-	CountryCode              string                 `json:"countryCode"`
-	InternationalRecipientNo string                 `json:"internationalRecipientNo"`
-	TemplateParameter        map[string]interface{} `json:"templateParameter"`
-	RecipientGroupingKey     string                 `json:"recipientGroupingKey,omitempty"`
+	RecipientNo              string                 `json:"recipientNo" validate:"max=50"`
+	CountryCode              string                 `json:"countryCode" validate:"max=8"`
+	InternationalRecipientNo string                 `json:"internationalRecipientNo" validate:"e164,max=20"`
+	TemplateParameter        map[string]interface{} `json:"templateParameter,omitempty" validate:"omitempty,max=20"`
+	RecipientGroupingKey     string                 `json:"recipientGroupingKey,omitempty" validate:"max=100"`
 }
